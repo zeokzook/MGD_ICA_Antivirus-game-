6d9eae9f530a47ce3c9594c2074c7e2bc804cf70
TAN <w9144603@tu54268.windows.tees.ac.uk>
2020-11-07 13:02:03 +0000

    Added player shooting and swipe up and down features to switch between 2 dimensions

--- MGD_ICA_Antivirus-game-/Antivirus/Antivirus/GameScene.swift
+++ MGD_ICA_Antivirus-game-/Antivirus/Antivirus/GameScene.swift
@@ -51,14 +51,79 @@ extension CGPoint
 
 class GameScene: SKScene
 {
-    let player = SKSpriteNode()
+    let player = SKSpriteNode(imageNamed: "BCell")
+    var moveAmtY: CGFloat = 0
+    var initPos: CGPoint = CGPoint.zero
+    var initTouch: CGPoint = CGPoint.zero
+    
+    let fireRate: Double = 1.5
+    let moveAmtthreshold: CGFloat = 100.0
     
     override func didMove(to view: SKView)
     {
+        backgroundColor = UIColor(displayP3Red: 0.545, green: 0, blue: 0, alpha: 1.0)
+        
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        
+        addChild(player)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if let touch = touches.first as UITouch?
+        {
+            initTouch = touch.location(in: self.scene!.view)
+            moveAmtY = 0
+            initPos = self.position
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if let touch = touches.first as UITouch?
+        {
+            let movingPoint: CGPoint = touch.location(in: self.scene!.view)
+            
+            moveAmtY = movingPoint.y - initTouch.y
+        }
         
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    //Shooting
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if moveAmtY < -moveAmtthreshold
+        {
+            print("up")
+        }
+        else if moveAmtY > moveAmtthreshold
+        {
+            print("down")
+        }
+        else
+        {
+            let projectile = SKSpriteNode(imageNamed:"Antibody")
+            projectile.position = player.position
+            
+            addChild(projectile)
+            
+            let shootAmount = projectile.position.x * 10
+            let realDest = CGPoint(x: shootAmount, y:0) + projectile.position
+            let actionRotate = SKAction.rotate(byAngle: -(CGFloat.pi)/2, duration: 0)
+            let actionMove = SKAction.move(to: realDest, duration: fireRate)
+            let actionMoveDone = SKAction.removeFromParent()
+            projectile.run(SKAction.sequence([actionRotate, actionMove, actionMoveDone]))
+            
+        }
+    }
+    
+    func switchDimension()
+    {
+        //switch player dimension
+    }
+    
+    func playerMovement()
+    {
+        //Player move up and down depending on gyroscope
     }
 }
